ce6388c244cdd46e0b731b5d4c7a7faa5de20133
Trung1711 <trannamtrung171199@gmail.com>
2020-06-12 06:22:29 +0700

    tuan 3
    tuan 3

---
+++ Tuan2/BankAccount.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tuan2
+{
+    class BankAccount
+    {
+        private string passWord;
+        private   bool isPasswordVerified;
+        private  int balance;
+        public string PassWord
+        {
+            get
+            {
+                return passWord;
+            }
+            set
+            {
+                passWord = value;
+            }
+
+        }
+        public  bool IsPasswordVerified
+        {
+            get
+            {
+                return isPasswordVerified;
+            }
+        }
+        public int Balance
+        {
+            get
+            {
+                CheckAccessPermission();
+                return balance;     
+            }
+            set
+            {
+                CheckAccessPermission();
+                balance = value;
+            }
+        }
+
+        public static double InterestRate { get; set; }
+        public UserInformation UserInf { get; set; }
+        public BankAccount(string username,int userage, uint userID, int balance, string passWord )
+        {
+            UserInformation.UserName = username;
+            UserInformation.UserAge = userage;
+            UserInformation.UserID = userID;
+            this.balance = balance;
+            this.passWord = passWord; 
+
+        }
+        static BankAccount()
+        {
+            InterestRate = 0.05;
+        }
+        public double CalculateInterstMoney()
+        {
+            return Balance * InterestRate;
+        }
+
+        public  void  VerifyPassWord(string password)
+        {
+            int a;
+            a = String.Compare(password, PassWord);
+            if (a == 0)
+                isPasswordVerified = true;
+            else
+                isPasswordVerified = false;
+        }
+        public  void CheckAccessPermission()
+        {
+            if (IsPasswordVerified == false)
+            {
+                //throw new AccessDeniedException("Nhap sai");  
+
+                AccessDeniedEvent(this);
+            }
+
+        }
+        
+        public delegate void AccessDeniedDelegate(BankAccount account);
+        public  event AccessDeniedDelegate AccessDeniedEvent;
+        public override string ToString()
+        {
+            return "Ten: " + UserInformation.UserName + ", Tuoi: " +UserInformation.UserAge + ", ID: " +UserInformation.UserID;
+        }
+    }
+}
---
+++ Tuan2/UserInformation.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tuan2
+{
+   public class UserInformation
+    {
+        private static string userName;
+        private static int userAge;
+        private static uint userID;
+        private double baseSalary;
+    
+        public static string UserName
+        {
+            get
+            {
+                return userName;
+            }
+            set
+            {
+                userName = value;
+            }
+        }
+        public static int UserAge
+        {
+            get
+            {
+                return userAge;
+            }
+            set
+            {
+                if(value <= 18)
+                {
+                    throw new UserAccountException("Error! User’s age must be larger than 18");
+                }
+                else
+                {
+                    userAge = value;
+                }
+               
+            }
+        }
+        public static uint UserID
+        {
+            get
+            {
+                return userID;
+            }
+            set
+            {
+                if(value <1000 || value >9999)
+                {
+                    throw new UserAccountException("Error! ID must be between 1000 and 9999");
+                }
+                else
+                {
+                   userID = value;
+                }
+            }
+        }
+        public override string ToString()
+        {
+            return "Ten: " + UserName + ", Tuoi: " + UserAge + ", ID: "+ UserID;
+        }
+
+        public UserInformation(string username, int userage, uint useriD)
+        {
+            UserName = username;
+            UserAge = userage;
+            UserID = useriD;
+        }
+        public double BaseSalary
+        {
+            get
+            {
+                return this.baseSalary;
+            }
+            set
+            {
+                this.baseSalary = value;
+            }
+        }
+        public UserInformation(string username,int userage,uint useriD, double baseSalary)
+        {
+            UserName = username;
+            UserAge = userage;
+            UserID = useriD;
+            BaseSalary = baseSalary;
+        }        
+        public virtual double GetSalary()
+        {
+            return BaseSalary;
+        }
+        
+    }
+}
